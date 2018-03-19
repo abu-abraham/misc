603c984fc60b34f87fa62b3590a7102b75efdbcb
Abu  Abraham <u6325322@anu.edu.au>
2018-03-20 09:57:18 +1100

    some moifications

--- Atlassian Prep/q4.py
+++ Atlassian Prep/q4.py
@@ -4,9 +4,32 @@ def answer(n, elements):
     for i in range(0,len(elements)-1):
         for j in range(min(elements[i],elements[i+1]),max(elements[i],elements[i+1])+1):
             A[j]+=1;
-    return A.index(max(A))
+    return A.index(max(A)) 
+
+def answer1(n,elements):
+    B = [0]*(n+2)
+    for i in range(0,len(elements)-1):
+        if elements[i] < elements[i+1]:
+            B[elements[i]]+=1;
+            B[elements[i+1]+1]-=1;
+        else:
+            B[elements[i]+1]-=1;
+            B[elements[i+1]]+=1;
+    max_val = 0
+    max_indexes = []
+    for i in range(1,len(B)):
+        B[i]+=B[i-1]
+        if B[i] >max_val:
+            max_val = B[i]
+            max_indexes = []
+            max_indexes.append(i);
+        elif B[i]==max_val:
+            max_indexes.append(i);
+    return min(max_indexes)
+            
 
 
 
 
-print answer(10,[1,5,10,3]);
+
+print answer1(10,[1,5,10,3]);
--- Atlassian Prep/q5.py
+++ Atlassian Prep/q5.py
@@ -1,31 +1,60 @@
 import math
 
+
+threshold = 1
+
+
 def getDivisors(n) :
     i =1;
     divisors = []
     while i <= math.sqrt(n):
-        if (n % i == 0 and i>1) :
+        if (n % i == 0) :
             if (n / i == i) :
                 divisors.append(i)
             else :
                 divisors.append(i)
                 divisors.append(n/i)
         i = i + 1
+    divisors = [i for i in divisors if i>threshold]
     return divisors
 
+def bfs(val_map,start):
+    visited, queue = set(), [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(val_map[vertex] - visited)
+    return list(visited)
+
 def answer(originList,destination):
-    threshold = 1
     value_map ={}
-    dest_map = {}
-    for item in originList:
+    total_list = originList+destination
+    total_list = list(set(total_list))
+    for item in total_list:
         if item > threshold: 
             org_facors = getDivisors(item)
-            for dest in destination:
-                if dest> threshold:
+            for dest in total_list:
+                if dest > threshold and dest != item:
                     dest_factors = getDivisors(dest)
                     common = list(set(org_facors) & set(dest_factors))
-                    dest_map[item] = common;
-    print dest_map
+                    if len(common)>0:
+                        value_map.setdefault(item,[]).append(dest)
+    for key in value_map:
+        value_map[key]=set(value_map[key])
+    dest_list = []
+    for index, org in enumerate(originList):
+        if org in value_map:
+            if destination[index] in bfs(value_map,org):
+                dest_list.append(1)
+        else:
+            dest_list.append(0)
+    print dest_list
+        
+
+
 
 print answer([1,2,4,6],[3,3,3,4])
+
+
 
